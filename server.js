f0e5b882644199b726d16bb7f652611b4c9e5b94
ADHEEN BHUVA <167649734+ADHEENBHUVA@users.noreply.github.com>
2026-08-11 09:34:01 +0530

    Updated project

--- server.js
+++ server.js
@@ -121,6 +121,10 @@ app.use('/api/digital-card', require('./routes/digitalCardRoutes'));
 
 // Error handling middleware can go here
 
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+    app.listen(PORT, () => {
+        console.log(`Server running on http://localhost:${PORT}`);
+    });
+}
+
+module.exports = app;
